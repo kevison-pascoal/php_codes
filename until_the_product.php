22235c0499ac724eaf5b9dfc95d18402b4a3bcce
kevison.developer@gmail.com <kevison.developer@gmail.com>
2022-08-19 09:34:34 -0300

    untilProductPHP:bulb:

---
+++ until_the_product.php
@@ -0,0 +1,46 @@
+<?php 
+  echo "\n\n=======================================================\n\n";
+  
+  final class UntilProduct {
+    private $number;
+    private $product;
+
+    public function __construct($number) {
+      $this->setNumber($number);
+      $this->setProduct();
+
+      for($count = 0; $count <= $this->product; $count++) {
+        echo $count." "; 
+      }
+    }
+
+    private function setNumber($number) {
+      $this->number = $number;
+    }
+    private function setProduct() {
+      $this->product = pow($this->getNumber(), 2);
+    }
+    private function getNumber() {
+      return $this->number;
+    }
+  }
+  final class Sun {
+    private $number;
+
+    public function __construct($number) {
+      $this->setNumber($number);
+      for($count = 0; $count < $number; $count++) {
+        echo "SOL";
+      }
+    }
+
+    private function setNumber($number) {
+      $this->number = $number;
+    }
+  }
+
+  $until = new UntilProduct(123);
+  echo "\n";
+  $sun = new Sun(0);
+  echo "\n\n=======================================================\n\n";
+?>
